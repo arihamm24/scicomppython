c6d5d4020c8791049e4b98cdacd425cd391399ce
Ari Hammond <ajhammond06@gmail.com>
2023-02-04 08:59:47 -0500

    Update lists.py

--- assignments/lists.py
+++ assignments/lists.py
@@ -9,3 +9,17 @@ for line in fh:
             lst.append(x)
 lst.sort()
 print(lst)
+
+#Pulling emails from document, ommitting duplicate properties
+fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
+count = 0
+
+fh = open(fname)
+for line in fh:
+    if line.startswith("From") and not line.startswith("From:"):
+        word = line.split()
+        count = count + 1
+        print(word[1])
+print("There were", count, "lines in the file with From as the first word")
